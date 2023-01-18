875a67ca3673a15846043bb249af9713c918dd45
tp-27 <91338909+tp-27@users.noreply.github.com>
2023-01-17 22:56:04 -0500

    Fixed duplicate map display

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const optionsContainer = document.querySelector(".options-container");
 const listOptions = document.querySelectorAll(".option");
 
 let map = document.querySelector("map");
-
+let mapStatus = "off";
 
 // Adding and removing options panel
 selected.addEventListener("click", () => {
@@ -18,23 +18,28 @@ listOptions.forEach(options => {
 
         // find id of university selected
         const input = options.querySelector(".radio");
+        isMapOpen();
         mapOpen(input.id);     
     })
 })
 
+function isMapOpen() {
+    if (mapStatus === "on") {
+        let currentMap = document.querySelector(".iframe");
+        body.removeChild(currentMap);
+        mapStatus = "off";
+    }
+}
+
 // Add action listener to <a> , retrieve which label is selected and display map
 function mapOpen(university) {
     // FIX ME: remove map if exists - displays two maps instead of one 
-
-        // if (map.hasChildNodes()) {
-        //     map.remove();
-        // }
-
     let googleMap = document.createElement("iframe");
-    googleMap.src = "https://www.google.com/maps/d/embed?mid=10R-9p0w9a0FVXovNiuJzrygB_3w&ehbc=2E312F";
+    googleMap.src = "https://www.google.com/maps/d/u/0/embed?mid=1SV3wkaPMxAM73jmbt37518-bN0wS7pg&ehbc=2E312F";
     googleMap.style.height = "480px";
     googleMap.style.width = "640px";
 
+    mapStatus = "on";
         // switch(university) {
         //     case "guelph": 
         //         map.src = "https://www.google.com/maps/d/u/0/viewer?mid=10R-9p0w9a0FVXovNiuJzrygB_3w&ll=43.534514775651104%2C-80.22755028221935&z=15";
@@ -67,5 +72,4 @@ function mapOpen(university) {
         // }
     
     document.body.appendChild(googleMap);
-
 }
