7d0dcc435483d6df017a5ccc5ea0d56fe584100d
tp-27 <91338909+tp-27@users.noreply.github.com>
2023-02-26 23:42:37 -0500

    updated ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# aed-me
+# AEDMe
+## About The Project
+### This application displays to the user all of the nearest AEDs found on a selected campus based on the users' current location. The project currently serves 7 different universities and plans to reach many other univerisites in Ontario. 
+
--- src/main.js
+++ src/main.js
@@ -29,88 +29,56 @@ listOptions.forEach(options => {
 
 
 
-function switchMap(university) {
-    let campus; 
+// function switchMap(university) {
+//     let campus; 
 
-    switch(university) {
-        case "guelph":
-            campus = { lat: 43.5327217, lng: -80.2261804 };
-            break;
+//     switch(university) {
+//         case "guelph":
+//             campus = { lat: 43.5327217, lng: -80.2261804 };
+//             break;
 
-        case "waterloo":
-            campus = { lat: 43.4723, lng: -80.5449 };
-            break;
+//         case "waterloo":
+//             campus = { lat: 43.4723, lng: -80.5449 };
+//             break;
 
-        case "laurier":
-            campus = { lat: 43.4738, lng: -80.5275 };
-            break;
+//         case "laurier":
+//             campus = { lat: 43.4738, lng: -80.5275 };
+//             break;
 
-        case "western":
-            campus = { lat: 43.0096, lng: -81.2737 };
-            break;
+//         case "western":
+//             campus = { lat: 43.0096, lng: -81.2737 };
+//             break;
 
-        case "mcmaster":
-            campus = { lat: 43.2609, lng: -79.91912};
-            break;
+//         case "mcmaster":
+//             campus = { lat: 43.2609, lng: -79.91912};
+//             break;
 
-        case "toronto":
-            campus = { lat: 43.6629, lng:  -79.3957 };
-            break;
+//         case "toronto":
+//             campus = { lat: 43.6629, lng:  -79.3957 };
+//             break;
     
-        case "brock":
-            campus = { lat: 43.1176, lng: -79.2477 };
-            break;
+//         case "brock":
+//             campus = { lat: 43.1176, lng: -79.2477 };
+//             break;
 
-    }
+//     }
 
-    map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15,
-        center: campus,
+//     map = new google.maps.Map(document.getElementById("map"), {
+//         zoom: 15,
+//         center: campus,
         
-    });
-
-    // geoCode();
-    // addMarkers(university); 
-}
-
-// Map functions
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 8,
-        center: { lat: -34.397, lng: 150.644 },
-        mapTypeControl: false,
-    });
-}
-
-// function geocode() {
-//     var location = '401 beaumont cres Kitchener, ON'; 
-//     axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
-//       params: {
-//         address: location,
-//         key: 'AIzaSyCzGghxXBLge_jHypQILU7-1evtXjBIUio'
-//       }
-//     })
-//     // After getting request response - then do something
-//     .then(function(response) {
-//       // Log full response
-//       console.log(response);
-
-//       // Formatted coordinates
-//       var coordinates = console.log(response.data.results[0].geometry.location.latitude);
-//     })
-//     .catch(function(error) {
-//       console.log(error);
 //     });
+
+//     // geoCode();
+//     // addMarkers(university); 
 // }
 
-// window.initMap = initMap();
-
-// function addMarkers(university) {
-//     geoCode();
-//     marker = new google.maps.Marker({
-//         position: {lat: 43.5294, lng: -80.2277},
-//         map,
-//         title: "Hello World!",
-//       });
+// // Map functions
+// function initMap() {
+//     map = new google.maps.Map(document.getElementById("map"), {
+//         zoom: 8,
+//         center: { lat: -34.397, lng: 150.644 },
+//         mapTypeControl: false,
+//     });
 // }
 
